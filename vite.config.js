f5409d362497e80b76a0b47c4b03b4a29e41ea31
HBDevProductions <haa43864@gmail.com>
2026-06-16 22:21:40 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import base44 from "@base44/vite-plugin";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), base44()],
 });
